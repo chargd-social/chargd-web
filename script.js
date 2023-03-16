4a502d40941eff603497a32eb354b0312d50f067
bucketfishy <bucketfishy@gmail.com>
2023-03-16 17:09:46 +0800

    laod feed on page load

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
+
+function load_feed() {
+  const show_div = document.getElementById("show_stuff");
+  show_div.innerHTML = "";
+
+
+  fetch('https://api.chargd.social/battery')
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    for (const username in data) {
+      const personDiv = document.createElement("div");
+
+      var text = ""
+
+      const plugin_text = data[username]["is_plugin"] == "true" ? "plugged in" : "unplugged";
+
+      const timestamp = format_time(data[username]["timestamp"])
+
+      text = `<div class="feed_item">
+      <p class="feed_timestamp">${timestamp}</p>
+      <h1 class="feed_username">${username}</h1>
+      <h2 class="feed_actiontext"> ${plugin_text} their phone.</h2>
+      <h1 class="feed_percentage">${data[username]["battery"]}%</h1>
+      <p class="feed_captionbox">${data[username]["caption"]}</p>
+      </div>
+      `
+
+      personDiv.innerHTML = text;
+
+      show_div.appendChild(personDiv);
+
+    }
+  })
+}
+
 document.querySelector('button')
-  .addEventListener('click', (e) => {
-    const show_div = document.getElementById("show_stuff");
-    show_div.innerHTML = "";
-
-    e.preventDefault(); // no idea what this does i copied it from somewhere
-
-    fetch('https://api.chargd.social/battery')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-
-        for (const username in data) {
-          const personDiv = document.createElement("div");
-
-          var text = ""
-
-          const plugin_text = data[username]["is_plugin"] == "true" ? "plugged in" : "unplugged";
-
-          const timestamp = format_time(data[username]["timestamp"])
-
-          text = `<div class="feed_item">
-            <p class="feed_timestamp">${timestamp}</p>
-            <h1 class="feed_username">${username}</h1>
-            <h2 class="feed_actiontext"> ${plugin_text} their phone.</h2>
-            <h1 class="feed_percentage">${data[username]["battery"]}%</h1>
-            <p class="feed_captionbox">${data[username]["caption"]}</p>
-          </div>
-          `
-
-          personDiv.innerHTML = text;
-
-          show_div.appendChild(personDiv);
-
-        }
-      })
-  });
+.addEventListener('click', (e) => {
+  e.preventDefault(); // no idea what this does i copied it from somewhere
+  load_feed()
+});
 
 
 
@@ -62,19 +67,19 @@ function format_time(unix) {
   var elapsed = current_date.getTime() - post_date.getTime();
 
   if (elapsed < msPerMinute) {
-       return Math.round(elapsed/1000) + ' seconds ago';
+    return Math.round(elapsed/1000) + ' seconds ago';
   }
 
   else if (elapsed < msPerHour) {
-       return Math.round(elapsed/msPerMinute) + ' minutes ago';
+    return Math.round(elapsed/msPerMinute) + ' minutes ago';
   }
 
   else if (elapsed < msPerDay ) {
-       return 'today at ' + formatted_post_time;
+    return 'today at ' + formatted_post_time;
   }
 
   else {
-      return month + ' ' + date + ' ' + formatted_post_time;
+    return month + ' ' + date + ' ' + formatted_post_time;
   }
 
 }
