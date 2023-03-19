5209bab017577c63c0449fb92f33c0ae4be370c1
bucketfishy <bucketfishy@gmail.com>
2023-03-19 12:51:28 +0800

    percentage plugged in bar

--- script.js
+++ script.js
@@ -1,44 +1,73 @@
 
+const DEBUG = false
+
+var document_root = document.querySelector(":root");
+
+
 function load_feed() {
   const show_div = document.getElementById("show_stuff");
   show_div.innerHTML = "";
 
+  var num_users = 0;
+  var plugged_in_users = 0;
 
-  fetch('https://api.chargd.social/battery')
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
 
-    for (const username in data) {
-      const personDiv = document.createElement("div");
+  if (!DEBUG){
+    fetch('https://api.chargd.social/battery')
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
 
-      var text = ""
+      for (const username in data) {
+        num_users += 1;
+        const personDiv = document.createElement("div");
 
-      const plugin_text = data[username]["is_plugin"] == "true" ? "plugged in" : "unplugged";
+        var text = ""
 
-      const timestamp = format_time(data[username]["timestamp"])
+        const plugin_text = data[username]["is_plugin"] == "true" ? "plugged in" : "unplugged";
+        plugged_in_users += (data[username]["is_plugin"] == "true") ? 1 : 0
 
-      text = `<div class="feed_item">
-      <p class="feed_timestamp">${timestamp}</p>
-      <h1 class="feed_username">${username}</h1>
-      <h2 class="feed_actiontext"> ${plugin_text} their phone.</h2>
-      <h1 class="feed_percentage">${data[username]["battery"]}%</h1>
-      <p class="feed_captionbox">${data[username]["caption"]}</p>
-      </div>
-      `
+        const timestamp = format_time(data[username]["timestamp"])
 
-      personDiv.innerHTML = text;
+        text = `<div class="feed_item">
+        <p class="feed_timestamp">${timestamp}</p>
+        <h1 class="feed_username">${username}</h1>
+        <h2 class="feed_actiontext"> ${plugin_text} their phone.</h2>
+        <h1 class="feed_percentage">${data[username]["battery"]}%</h1>
+        <p class="feed_captionbox">${data[username]["caption"]}</p>
+        </div>
+        `
 
-      show_div.appendChild(personDiv);
+        personDiv.innerHTML = text;
 
-    }
-  })
+        show_div.appendChild(personDiv);
+      }
+
+      set_circle_percentage((plugged_in_users / num_users) * 100);
+    })
+  }
+  else {
+
+    show_div.innerHTML = `
+    <div>
+    <div class="feed_item">
+    <p class="feed_timestamp">today at 00:00am</p>
+    <h1 class="feed_username">PERSON!</h1>
+    <h2 class="feed_actiontext"> plugged in their phone.</h2>
+    <h1 class="feed_percentage">90%</h1>
+    <p class="feed_captionbox">some caption</p>
+    </div>
+    </div>
+    `;
+  }
+
 }
 
 
 window.onload = (event) => {
+  set_circle_percentage(100);
   load_feed();
 };
 
@@ -48,6 +77,42 @@ document.querySelector('button')
   e.preventDefault(); // no idea what this does i copied it from somewhere
   load_feed()
 });
+
+
+
+
+
+
+
+function set_circle_percentage(val) { // percent from 0-100
+
+  const circle_radius = 120;
+  const circle_circum = Math.PI*(circle_radius*2);
+
+  if (val < 0) { val = 0;}
+  if (val > 100) { val = 100;}
+
+  var percent = ((100-val)/100)*circle_circum;
+
+  document_root.style.setProperty("--progress-bar-percent", percent);
+  console.log(percent)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
