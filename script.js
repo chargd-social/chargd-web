6418be835be2e9b54552cc90cb1e727e3dbce04c
bucketfishy <bucketfishy@gmail.com>
2023-03-19 16:22:06 +0800

    no flashing while refresh

--- script.js
+++ script.js
@@ -1,73 +1,69 @@
 
-const DEBUG = false
+const DEBUG = false // this doesn't do anything rn but maybe it could
+
+var api_url = "https://api.chargd.social" // can change this during debug
+api_url = "http://localhost:3000"
+
 
 var document_root = document.querySelector(":root");
 
 
+// fetch the feed and laod it into the page
 function load_feed() {
   const show_div = document.getElementById("show_stuff");
-  show_div.innerHTML = "";
+  const new_div = document.createElement("div");
+  // show_div.innerHTML = "";
 
+  // keep track of number of users to use to set the circular progress bar
   var num_users = 0;
   var plugged_in_users = 0;
 
+  fetch(api_url + '/battery')
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
 
-  if (!DEBUG){
-    fetch('https://api.chargd.social/battery')
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
+    // add each user
+    for (const username in data) {
+      num_users += 1;
+      const personDiv = document.createElement("div");
 
-      for (const username in data) {
-        num_users += 1;
-        const personDiv = document.createElement("div");
+      var text = ""
 
-        var text = ""
+      const plugin_text = data[username]["is_plugin"] == "true" ? "plugged in" : "unplugged";
+      plugged_in_users += (data[username]["is_plugin"] == "true") ? 1 : 0
 
-        const plugin_text = data[username]["is_plugin"] == "true" ? "plugged in" : "unplugged";
-        plugged_in_users += (data[username]["is_plugin"] == "true") ? 1 : 0
+      const timestamp = format_time(data[username]["timestamp"])
 
-        const timestamp = format_time(data[username]["timestamp"])
+      text = `<div class="feed_item">
+      <p class="feed_timestamp">${timestamp}</p>
+      <h1 class="feed_username">${username}</h1>
+      <h2 class="feed_actiontext"> ${plugin_text} their phone.</h2>
+      <h1 class="feed_percentage">${data[username]["battery"]}%</h1>
+      <p class="feed_captionbox">${data[username]["caption"]}</p>
+      </div>
+      `
 
-        text = `<div class="feed_item">
-        <p class="feed_timestamp">${timestamp}</p>
-        <h1 class="feed_username">${username}</h1>
-        <h2 class="feed_actiontext"> ${plugin_text} their phone.</h2>
-        <h1 class="feed_percentage">${data[username]["battery"]}%</h1>
-        <p class="feed_captionbox">${data[username]["caption"]}</p>
-        </div>
-        `
+      personDiv.innerHTML = text;
 
-        personDiv.innerHTML = text;
+      new_div.appendChild(personDiv);
+    }
 
-        show_div.appendChild(personDiv);
-      }
+    // update the div
+    show_div.innerHTML = "";
+    show_div.appendChild(new_div);
 
-      set_circle_percentage((plugged_in_users / num_users) * 100);
-    })
-  }
-  else {
+    // update the circle percentage bar
+    set_circle_percentage((plugged_in_users / num_users) * 100);
+  })
 
-    show_div.innerHTML = `
-    <div>
-    <div class="feed_item">
-    <p class="feed_timestamp">today at 00:00am</p>
-    <h1 class="feed_username">PERSON!</h1>
-    <h2 class="feed_actiontext"> plugged in their phone.</h2>
-    <h1 class="feed_percentage">90%</h1>
-    <p class="feed_captionbox">some caption</p>
-    </div>
-    </div>
-    `;
-  }
 
 }
 
 
 window.onload = (event) => {
-  set_circle_percentage(100);
   load_feed();
 };
 
@@ -84,18 +80,22 @@ document.querySelector('button')
 
 
 
-function set_circle_percentage(val) { // percent from 0-100
+function set_circle_percentage(val) { // set the percentage on the circle progress bar for percent of users connected
 
+  // constants — taken from the svg
   const circle_radius = 120;
   const circle_circum = Math.PI*(circle_radius*2);
 
+  // clamp value
   if (val < 0) { val = 0;}
   if (val > 100) { val = 100;}
 
+  // calculate length of path
   var percent = ((100-val)/100)*circle_circum;
 
+  // set length of path
   document_root.style.setProperty("--progress-bar-percent", percent);
-  console.log(percent)
+  console.log(percent) // debug
 
 }
 
