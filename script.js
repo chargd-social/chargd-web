25fc4bfff7f08c5aa27474a23ed01c4691105801
bucketfishy <bucketfishy@gmail.com>
2023-03-19 20:02:16 +0800

    show battery icons for visualisation

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ var document_root = document.querySelector(":root");
 function load_feed() {
   const show_div = document.getElementById("show_stuff");
   const new_div = document.createElement("div");
-  // show_div.innerHTML = "";
+
+  const show_battery = document.getElementById("temp-battery-display")
+  const battery_div = document.createElement("div"); // to show the battery visualisation
 
   // keep track of number of users to use to set the circular progress bar
   var num_users = 0;
@@ -29,6 +31,7 @@ function load_feed() {
     for (const username in data) {
       num_users += 1;
       const personDiv = document.createElement("div");
+      const new_battery = document.createElement("div");
 
       var text = ""
 
@@ -49,11 +52,46 @@ function load_feed() {
       personDiv.innerHTML = text;
 
       new_div.appendChild(personDiv);
+
+
+
+      text = `
+
+				<p class="battery-username">${username}</p>
+
+
+				<div class="battery-display">
+					<svg viewBox="0 0 84 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<rect class="battery-stroke" x="2" y="2" width="76" height="36" rx="6" stroke-width="4"/>
+						<path class="battery-stroke battery-fill" d="M78 14H80C81.1046 14 82 14.8954 82 16V25C82 26.1046 81.1046 27 80 27H78V14Z" stroke-width="4"/>
+						<path class="battery-chargeindicator" d="M78.07 21.3421L79.8184 15.1018C79.9125 14.766 80.4072 14.834 80.4072 15.1828V19.575C80.4072 19.7407 80.5415 19.875 80.7072 19.875H81.8308C82.0948 19.875 82.2864 20.1263 82.2166 20.3809L80.4399 26.8532C80.3473 27.1904 79.8506 27.1235 79.8506 26.7738V22.15C79.8506 21.9843 79.7163 21.85 79.5506 21.85H78.4552C78.1903 21.85 77.9985 21.5972 78.07 21.3421Z"/>
+					</svg>
+
+					<div class="battery-inside">
+						<p class="battery-percentage">${data[username]["battery"]}%</p>
+						<p class="battery-time">${timestamp}</p>
+					</div>
+				</div>
+
+
+      `
+
+      new_battery.innerHTML = text;
+      new_battery.classList.add("battery")
+      if (data[username]["is_plugin"] == "true") {
+        new_battery.classList.add("plugged-in")
+      }
+
+      battery_div.appendChild(new_battery)
     }
 
     // update the div
     show_div.innerHTML = "";
     show_div.appendChild(new_div);
+
+
+    show_battery.innerHTML = "";
+    show_battery.appendChild(battery_div);
 
     // update the circle percentage bar
     set_circle_percentage((plugged_in_users / num_users) * 100);
